83830332bef998794ce73f14d3a8915743ef69c4
jodehirons <1019699245@qq.com>
2023-11-12 15:24:12 +0800

    重构文件-删除所有文件
    为了重构版本、先删除库中所有文件

--- Assets/Cainos/Third Party/Lucid Editor/Runtime/Attributes/ShowInInspectorAttribute.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Cainos.LucidEditor
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property)]
-    public class ShowInInspectorAttribute : Attribute { }
-}
