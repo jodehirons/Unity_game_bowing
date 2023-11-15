24d9f22e204d9ea3793678eefa64cb1fd509f455
jodehirons <1019699245@qq.com>
2023-11-15 19:49:00 +0800

    Revert "Merge pull request #24 from MisMOONMORE/develop"
    This reverts commit a9f4c33290e0d5cfc03d1fdfafc34cac73effa4d, reversing
    changes made to 56d059e5adc3fcd87cb14a92aef99a63931a7174.

--- Assets/MapScript/MapGenerate.cs
+++ Assets/MapScript/MapGenerate.cs
@@ -6,7 +6,7 @@ using UnityEngine.Pool;
 
 public class MapGenerate : MonoBehaviour
 {
-    [Header("游戏组件")]
+    [Header("外部属性")]
     public Transform playerTransform;
     public GameObject LeftMap;
     public GameObject RightMap;
@@ -14,16 +14,13 @@ public class MapGenerate : MonoBehaviour
     public GameObject[] Obstacles;
     public GameObject[] Bloods;
     public AudioSource BackMusic;
-    [Header("控制参数")]
+    [Header("控制属性")]
     public float Y_Position = 20.5f;
     public float controlObstacle = 0;
     public float controlBlood = 0;
     public float controlWall = 0;
     public float controlStart = 0;
 
-    /// <summary>
-    /// 在游戏开始前调用，用于初始化地图和播放背景音乐
-    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
@@ -32,9 +29,6 @@ public class MapGenerate : MonoBehaviour
         BackMusic.Play();   
     }
 
-    /// <summary>
-    /// 在每一帧中调用，用于生成地图
-    /// </summary>
     // Update is called once per frame
     void Update()
     {
@@ -42,9 +36,6 @@ public class MapGenerate : MonoBehaviour
     }
 
   
-    /// <summary>
-    /// 生成地图
-    /// </summary>
     void generateMap()
     {
         if(playerTransform.position.y + 18 > Y_Position)
@@ -67,27 +58,26 @@ public class MapGenerate : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// 生成其他游戏组件
-    /// </summary>
-    /// <param name="Y">生成物体的起始高度</param>
     void generateObject(float Y)
     {
-        for (float i = Y + 2, k = 0; i < Y + 18; i += 3)
+        int flag = 0;
+        for (float i = Y + 1, k = 0; i < Y + 18; i += 1.5f, flag++)
         {
-            float[] arr = {-5.4f, -1.5f, 2f, 5.4f };
-            bool[] index = new bool[4];
-            int mem = 2;
-            while(mem > 0)
+            
+            float[] arr = {-6f,-4.5f, -3f, -1.5f, 0, 1.5f, 3f, 4.5f, 6f};
+            bool[] index = new bool[9];
+            int mem = 3;
+            if (flag % 2 == 1) mem = 1;
+            while (mem > 0)
             {
-                int j = Random.Range(0, 4);
+                int j = Random.Range(0, 1000)%9;
                 if(adjust(index, j))
                 {
                     index[j] = true;
                     mem--;
                 }
             }
-            for(int j = 0; j < 4; j++)
+            for(int j = 0; j < 9; j++)
             {
                 if (index[j] && k % 2 == 0)
                 {
@@ -97,11 +87,12 @@ public class MapGenerate : MonoBehaviour
                 {
                     generateWall(arr[j], i);
                 }
+                k++;
             }
-            k++;
-            if(i == Y+8)
+            
+            if(i == Y+4 || i == Y + 13)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     if (!index[j])
                     {
@@ -115,21 +106,12 @@ public class MapGenerate : MonoBehaviour
        
     }
 
-    /// <summary>
-    /// 根据给定的布尔数组和索引，判断该索引对应的布尔值是否为真（该格是否被占用）
-    /// </summary>
-    /// <param name="a">布尔数组</param>
-    /// <param name="b">索引</param>
-    /// <returns>如果该索引对应的布尔值为真，则返回 false，否则返回 true</returns>
     bool adjust(bool[] a, int b)
     {
         if (a[b]) return false;
         return true;
     }
 
-    /// <summary>
-    /// 初始化地图
-    /// </summary>
     void map_Init()
     {
         for (int i = 0; i < 7; i++) Obstacles[i].active = false;
@@ -151,52 +133,22 @@ public class MapGenerate : MonoBehaviour
 
     }
 
-    /// <summary>
-    /// 获取指定索引位置的障碍物
-    /// 如果该位置没有障碍物，则将所有障碍物激活并返回原始索引
-    /// </summary>
-    /// <param name="x">指定的索引位置</param>
-    /// <returns>指定索引位置的障碍物</returns>
     int GetIndex(int x)
     {
-        for(int i = 0; i < 6; i++)
-        {
-            x %= 6;
-            if (Obstacles[x].active)
-            {
-                return x;
-            }
-        }
-
-        for (int i = 0; i < 6; i++)
-        {
-            Obstacles[i].active = true;
-        }
-
+        if (Obstacles[x].active) return x;
+        Obstacles[x].active = true;
         return x;
     }
 
-    /// <summary>
-    /// 生成普通障碍物
-    /// </summary>
-    /// <param name="x">生成位置的x坐标</param>
-    /// <param name="y">生成位置的y坐标</param>
     void generateObstacle(float x, float y)
     {
         
         Vector3 a = new Vector3(x, y, 0);
-        int indexa = GetIndex(Random.Range(0, 6));
-        Obstacles[indexa].active = true;
+        int indexa = GetIndex(Random.Range(0, 1000)%6);
         Instantiate(Obstacles[indexa], a, Quaternion.identity);
         
     }
 
-
-    /// <summary>
-    /// 生成血包
-    /// </summary>
-    /// <param name="x">生成位置的x坐标</param>
-    /// <param name="y">生成位置的y坐标</param>
     void generateBlood(float x, float y)
     {
         Vector3 a = new Vector3(x, y, 0);
@@ -205,14 +157,9 @@ public class MapGenerate : MonoBehaviour
         Instantiate(Bloods[indexa], a, Quaternion.identity);
     }
     
-    /// <summary>
-    /// 生成地图边界
-    /// </summary>
-    /// <param name="x">生成位置的x坐标</param>
-    /// <param name="y">生成位置的y坐标</param>
     void generateWall(float x, float y)
     {
-        int t = Random.Range(5, 9);
+        int t = Random.Range(3, 100)%9;
         int[,] arr = new int[3, 3]; 
         for(float i = 0; i < t; )
         {
@@ -231,7 +178,9 @@ public class MapGenerate : MonoBehaviour
             {
                 if (arr[i,j] == 0)
                 {
-                    Vector3 a = new Vector3(x-1+i, y-1+j, 0);
+                    float ina = i;
+                    float inb = j;
+                    Vector3 a = new Vector3(x-0.5f+ina/2, y-0.5f+inb/2, 0);
                     Obstacles[6].active = true;
                     Instantiate(Obstacles[6], a, Quaternion.identity);
                 }
--- Assets/MapScript/RawBlood.cs
+++ Assets/MapScript/RawBlood.cs
@@ -4,9 +4,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// RawBlood类用于处理血条的显示
-/// </summary>
 public class RawBlood : MonoBehaviour
 {
     public RawImage rawImage; // 这是Raw Image组件的引用  
@@ -14,30 +11,20 @@ public class RawBlood : MonoBehaviour
     public float head;
     public TextMeshProUGUI headText;
 
-    /// <summary>
-    /// 初始化血条
-    /// </summary>
     void Start()
     {
         head = 826f;
     }
 
-    /// <summary>
-    /// 每一帧调用血条更新
-    /// </summary>
     private void Update()
     {
         Myblood();
     }
 
-    /// <summary>
-    /// 更新血条的显示
-    /// </summary>
     void Myblood()
     {
         float health = rawImage.rectTransform.sizeDelta.x;
         rawImage.rectTransform.anchoredPosition = new Vector2(health / 2 - head, rawImage.rectTransform.anchoredPosition.y);
-        Debug.Log(head);
         float t = health / 60;
         headText.text = t.ToString();
         
